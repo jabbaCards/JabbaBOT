057249c4e8143a1162c86292d7cb13dc3903f266
jabbaCards <fellipe.cuestas@gmail.com>
2026-07-02 16:30:13 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
-print("JabbaBOT iniciado com sucesso!")
+import os
+import requests
+
+TOKEN = os.environ["TELEGRAM_TOKEN"]
+CHAT_ID = os.environ["CHAT_ID"]
+
+mensagem = """
+🚀 JabbaBOT está ONLINE!
+
+✅ Teste realizado com sucesso.
+
+Em breve este grupo receberá:
+🔥 Ofertas de Pokémon TCG
+🎟️ Cupons
+💰 Links de afiliado
+📦 Produtos do Jabba Cards
+"""
+
+url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+
+requests.post(
+    url,
+    json={
+        "chat_id": CHAT_ID,
+        "text": mensagem
+    }
+)
+
+print("Mensagem enviada!")
