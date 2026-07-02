5b3be29fe8f2908f679c8db0a721738b1396dd5a
jabbaCards <fellipe.cuestas@gmail.com>
2026-07-02 17:35:10 -0300

    main.py

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("🚀 BOT ESTÁ RODANDO")
+print("🚀 INICIOU O BOT")
+
+import os
+
+print("TOKEN EXISTE:", "TELEGRAM_TOKEN" in os.environ)
+print("CHAT_ID EXISTE:", "CHAT_ID" in os.environ)
+
+print("🚀 FINAL DO TESTE")
