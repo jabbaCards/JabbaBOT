9ae44d193a1702507977989ec3fbc5f065210076
jabbaCards <fellipe.cuestas@gmail.com>
2026-07-02 17:04:58 -0300

    main.py

--- main.py
+++ main.py
@@ -4,11 +4,14 @@ import requests
 TOKEN = os.environ["TELEGRAM_TOKEN"]
 CHAT_ID = os.environ["CHAT_ID"]
 
+print("TOKEN OK:", bool(TOKEN))
+print("CHAT_ID:", CHAT_ID)
+
 url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
 
 payload = {
     "chat_id": CHAT_ID,
-    "text": "🚨 TESTE DEFINITIVO DO BOT"
+    "text": "🚨 TESTE DEFINITIVO DO BOT - SE ISSO NÃO CHEGAR, É TOKEN OU CHAT_ID"
 }
 
 r = requests.post(url, json=payload)
