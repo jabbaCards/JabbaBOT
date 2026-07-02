a7ea26a8fca81a1657d238b9cdd067fe16ca417e
jabbaCards <fellipe.cuestas@gmail.com>
2026-07-02 18:54:40 -0300

    main.py

--- main.py
+++ main.py
@@ -4,26 +4,28 @@ import requests
 TOKEN = os.environ["TELEGRAM_TOKEN"]
 CHAT_ID = os.environ["CHAT_ID"]
 
-# URL da Versão 9
-URL_DO_GOOGLE = "https://script.google.com/macros/s/AKfycbwBBK4uH5H1EtUNrXtUPM_QZ4BjuaPrGastz3_oUb_OJm-Ng-7o-4LMmf_YUtnwERgI/exec"
+# URL da Versão 11
+URL_DO_GOOGLE = "https://script.google.com/macros/s/AKfycbzsRvHhyduZmnmpZEoS9jm9N0WBxsCCtGna7bA1XMo0PtQ6_tU6bpkcKnqIy5ocREMv/exec"
 
 def main():
     try:
-        # Aumentamos o timeout para garantir que o script tenha tempo de buscar
-        resposta = requests.get(f"{URL_DO_GOOGLE}?q=pokemon", timeout=40)
+        # Busca os dados processados pelo Script
+        resposta = requests.get(f"{URL_DO_GOOGLE}?q=pokemon", timeout=30)
         dados = resposta.json()
         
-        titulo = dados.get('titulo', 'Sem título')
+        titulo = dados.get('titulo', 'Produto não capturado')
         preco = dados.get('preco', '0')
         
-        mensagem = f"🔥 *JABBABOT ATIVO (Versão 9)* 🔥\n\n📦 {titulo}\n💰 R$ {preco}"
+        # Envia para o Telegram
+        mensagem = f"🔥 *JABBABOT ATIVO (Versão 11)* 🔥\n\n📦 {titulo}\n💰 R$ {preco}"
         
         requests.post(f"https://api.telegram.org/bot{TOKEN}/sendMessage", 
                       json={"chat_id": CHAT_ID, "text": mensagem})
-        print("✅ Sucesso! Mensagem enviada.")
+        
+        print("✅ Execução concluída.")
         
     except Exception as e:
-        print(f"❌ Erro final: {e}")
+        print(f"❌ Erro na execução: {e}")
 
 if __name__ == "__main__":
     main()
